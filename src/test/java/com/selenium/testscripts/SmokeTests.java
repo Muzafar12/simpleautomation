9311dc79ab3f323dcc693901ff3e07bfe69776d9
Muzafar <taranqi1214@gmail.com>
2022-09-20 20:58:33 -0400

    this is  too much

--- src/test/java/com/selenium/testscripts/SmokeTests.java
+++ src/test/java/com/selenium/testscripts/SmokeTests.java
@@ -1,10 +1,13 @@
 package com.selenium.testscripts;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
+
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
 
 
 
@@ -15,20 +18,20 @@ public class SmokeTests{
 	@Test
 	public void testOne() throws Exception {
 		
+		TestScreenRecorder.startRecording("navigationTest"); 
+		
 		WebDriver driver = new SafariDriver(); 		// Launch Website 
-		driver.navigate().to("http://www.google.com/"); 
-
-		// Click on the search text box and send value 
-		WebElement searchbox = driver.findElement(By.name("q"));
-		searchbox.sendKeys("BrowserStack");
-		// Click on the search button 
-		WebElement searchbtn = driver.findElement(By.name("btnK"));
-		searchbtn.click(); 
-		
-		Thread.sleep(5000);
-		// Close the Browser 
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/"); 
+		driver.manage().window().maximize();
+		WebElement username = driver.findElement(By.name("txtUsername"));
+		username.sendKeys("Admin");
+		WebElement password = driver.findElement(By.name("txtPassword"));
+		password.sendKeys("admin123"); 
+		WebElement login = driver.findElement(By.name("Submit"));
+		login.click();
+		System.out.println(driver.getTitle());
 		driver.quit();
-		
+		TestScreenRecorder.stopRecording();
 	}
 
 }
